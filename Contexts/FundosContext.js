a7f1c0af01787928b7896d3b62b834f17827b892
yangshindo <eliwood182@hotmail.com>
2022-10-26 16:47:35 -0300

    debug custom chart values

--- Contexts/FundosContext.js
+++ Contexts/FundosContext.js
@@ -4,9 +4,9 @@ export const FundosContext = createContext();
 
 function FundosContextProvider(props) {
   const [fundosDBList, setFundosDBList] = useState([
-    { nome: "BLMG11", rendimento: 0.8, recebido: 8.0, cotas: 1, pago: true },
-    { nome: "CPTS11", rendimento: 1.1, recebido: 11.0, cotas: 1, pago: true },
-    { nome: "HGRU11", rendimento: 0.82, recebido: 8.2, cotas: 1, pago: false },
+    { nome: "BLMG11", rendimento: 0.8, recebido: 8.0, cotas: 1, pago: true, janeiro: 1, fevereiro: 1.20, marco: 1.22, abril: 1.33, maio: 0.7, junho: 0.4 },
+    { nome: "CPTS11", rendimento: 1.1, recebido: 11.0, cotas: 1, pago: true, janeiro: 0.2, fevereiro: 0.2, marco: 0.2, abril: 0.3, maio: 0, junho: 0.4 },
+    { nome: "HGRU11", rendimento: 0.82, recebido: 8.2, cotas: 1, pago: false, janeiro: 0.7, fevereiro: 0.2, marco: 0.9, abril: 2.3, maio: 2, junho: 1.4  },
   ]);
 
   const [fundosUserList, setFundosUserList] = useState([]);
--- Pages/Graficos.js
+++ Pages/Graficos.js
@@ -1,57 +1,54 @@
-import { Text, View, StyleSheet, Picker } from "react-native";
+import { Text, View, StyleSheet, Picker, Button } from "react-native";
 import { useState, useContext } from "react"
 import { LineChart } from "react-native-chart-kit";
 import { FundosContext } from "../Contexts/FundosContext";
 import BackgroundColorProvider from "../Components/BackgroundColorProvider";
+import { select } from "@react-native-material/core";
 
 function Graficos() {
-  const styles = StyleSheet.create({
-    body: {
-      margin: 10,
-      flexDirection: "column",
-      alignItems: "center",
-      flex: 1,
-    },
-    title: {
-      margin: 10,
-      fontSize: 30,
-      fontWeight: 500,
-      textAlign: "center",
-      color: "#104a07",
-    },
-  });
+
+  const { fundosUserList, fundosDBList } = useContext(FundosContext)
 
   const [selectedValue, setSelectedValue] = useState();
+  const [chartValue, setChartValue] = useState()
 
-  const fundosUserList = useContext(FundosContext)
+  const selectOption = fundosUserList.map((fundo) => <Picker.Item label={fundo.nome} value={fundo.nome} key={fundo.nome} />)
 
-  const fundosArray = fundosUserList.toArray()
-  const selectOption = fundosArray.map((fundo) => <Picker.Item lab={fundo.nome} value={fundo.nome} />)
 
   function SelectorPicker() {
     return(
         <View >
         <Picker
-          selectedValue={selectedValue}
+          selected={selectedValue}
           style={{ height: 30, width: 150}}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}
         >
-            {selectOption}
+           {selectOption}
         </Picker>
+        <Button onPress={chartGenerator}></Button>
       </View>
     )
   }
 
+  function chartGenerator(itemValue) {
+    const chartItem = (fundosDBList.find((item) => item.nome === selectedValue))
+    setChartValue([chartItem.janeiro, chartItem.fevereiro, chartItem.marco, chartItem.abril, chartItem.maio, chartItem.junho])
+    console.log(chartValue[0])
+  }
+
+
+
   return (
     <BackgroundColorProvider>
     <View style={styles.body}>
+      <SelectorPicker />
       <Text style={styles.title}>Histórico de Rendimento</Text>
       <LineChart
         data={{
           labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun"],
           datasets: [
             {
-              data: [3, 4.31, 7, 5, 0, 3],
+              data: [1, 2, 3, 4, 5, 6]
             },
           ],
         }}
@@ -78,5 +75,22 @@ function Graficos() {
     </BackgroundColorProvider>
   );
 }
+
+
+const styles = StyleSheet.create({
+  body: {
+    margin: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    flex: 1,
+  },
+  title: {
+    margin: 10,
+    fontSize: 30,
+    fontWeight: 500,
+    textAlign: "center",
+    color: "#104a07",
+  },
+});
 
 export default Graficos;
